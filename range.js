adf087a8c93a82ee1965776de832bf2aa59fa685
Julius Šakalys <julius.sakalys@gmail.com>
2017-07-13 01:48:03 +0300

    Incomplete

--- range.js
+++ range.js
@@ -1,140 +1,140 @@
-var picker = document.querySelector('.sakalys-date-range');
+(function() {
+  var picker = document.querySelector('.sakalys-date-range');
 
-var from = picker.querySelector('.skl-from'),
+  var from = picker.querySelector('.skl-from'),
     fromHandle = from.querySelector('.handle');
 
-var screenSize = screen.height;
-
-var realChange = 0;
-
-console.log('Screen height', screenSize);
-
-var to = picker.querySelector('.skl-to'),
+  var to = picker.querySelector('.skl-to'),
     toHandle = to.querySelector('.handle');
 
-var out = document.querySelector('.testFrom');
+  var out = document.querySelector('.testFrom');
 
-var fromPressed = false,
-  toPressed = false;
+  var daysFromToday = 0,
+    moveTotal = 0,
+    stackedMove = 0,
+    acceleratorInterval = null,
+    currentPosition;
 
-var lastPos = -1,
-  change = 0;
+  function setupHandleElement(el) {
+    function stop(e) {
+      e.preventDefault();
+    }
+    el.addEventListener('dragstart', stop);
+    el.addEventListener('touchstart', stop);
+  }
 
-function setupHandle (el) {
-  var stop = function (e) {
-    e.preventDefault();
-  };
-  el.addEventListener('dragstart', stop);
-  el.addEventListener('touchstart', stop);
-}
+  setupHandleElement(fromHandle);
+  setupHandleElement(toHandle);
 
-setupHandle(fromHandle);
-setupHandle(toHandle);
+  function pressedCb(e) {
+    enableScroll(e);
+  }
+  function unpressedCb(e) {
+    disableScroll(e);
+  }
 
-var fromDownCb = function (e) {
-  fromPressed = true;
-  enableScroll(e);
-};
-var fromUpCb = function () {
-  fromPressed = false;
-  disableScroll();
-};
-var toDownCb = function (e) {
-  toPressed = true;
-  enableScroll(e);
-};
-var toUpCb = function () {
-  toPressed = true;
-  disableScroll();
-};
+  fromHandle.addEventListener('mousedown', pressedCb);
+  fromHandle.addEventListener('touchstart', pressedCb);
 
-// FROM
-// down
-fromHandle.addEventListener('mousedown', fromDownCb);
-fromHandle.addEventListener('touchstart', fromDownCb);
-// up
-fromHandle.addEventListener('mouseup', fromUpCb);
-fromHandle.addEventListener('touchend', fromUpCb);
-fromHandle.addEventListener('touchcancel', fromUpCb);
 
-// TO
-// down
-toHandle.addEventListener('mousedown', toDownCb);
-toHandle.addEventListener('touchstart', toDownCb);
-// up
-toHandle.addEventListener('mouseup', toUpCb);
-toHandle.addEventListener('touchend', toUpCb);
-toHandle.addEventListener('touchcancel', toUpCb);
+  function prepareDate(dayDiff) {
+    var date = new Date();
 
-function applyChange(date, change) {
-  date.setTime(date.getTime() + (change * 1000 * 60 * 60 * 24));
+    date.setTime(date.getTime() + (dayDiff * 1000 * 60 * 60 * 24));
 
-  return date;
-}
-var moveListener = function (e) {
+    return date;
+  }
 
-  var pos;
-  if (e.touches) {
-    pos = e.touches[0].screenY;
-  } else {
-    if (e.which === 0) {
-      fromPressed = false;
-      toPressed = false;
+  function moveListener (e) {
+
+    currentPosition = e.screenY;
+
+    moveTotal += stackedMove;
+    stackedMove = 0;
+
+    daysFromToday += (moveTotal / 60);
+    console.log(moveTotal);
+    // console.log(daysFromToday);
+
+
+    if (daysFromToday > 0) {
+      daysFromToday = Math.floor(daysFromToday);
+    } else {
+      daysFromToday = Math.ceil(daysFromToday);
+    }
+
+    out.innerHTML = formatDate(prepareDate(daysFromToday));
+  }
+
+  function formatDate(date) {
+    return paddy(date.getDate(), 2)
+      + "/"
+      + paddy((date.getMonth() + 1), 2)
+      + "/"
+      + date.getFullYear();
+  }
+
+  function paddy(subject, length, padChar) {
+    var pad_char = typeof padChar !== 'undefined' ? padChar : '0';
+    var pad = new Array(1 + length).join(pad_char);
+    return (pad + subject).slice(-pad.length);
+  }
+
+  function enableScroll(e) {
+    document.addEventListener('mouseup', unpressedCb);
+    document.addEventListener('touchend', unpressedCb);
+    document.addEventListener('touchcancel', unpressedCb);
+    out.innerHTML = formatDate(prepareDate(daysFromToday));
+
+    currentPosition = e.screenY;
+
+    document.addEventListener('mousemove', moveListener);
+    document.addEventListener('touchmove', moveListener);
+    startAccelerator();
+  }
+
+  function disableScroll() {
+    document.removeEventListener('mousemove', moveListener);
+    document.removeEventListener('touchmove', moveListener);
+    stopAccelerator();
+  }
+
+  function startAccelerator() {
+
+    function getCurrentPosition() {
+      return currentPosition;
+    }
+
+    var samplesPerSecond = 10;
+
+    var previousPos = getCurrentPosition();
+
+    acceleratorInterval = setInterval(function () {
+      var accelerate = 1;
+      var diff = previousPos - getCurrentPosition();
+
+      var absDiff = Math.abs(diff);
+
+      if (absDiff < 9) {
+        accelerate = 1;
+      } else if (absDiff < 16) {
+        accelerate = 2;
+      } else if (absDiff < 20) {
+        accelerate = 20;
+      } else if (absDiff < 30) {
+        accelerate = 40;
+      }
+
+      stackedMove += accelerate * (diff/100);
+    }, 1000 / samplesPerSecond)
+  }
+
+  function stopAccelerator() {
+    if (!acceleratorInterval) {
       return;
     }
-    pos = e.screenY;
+
+    clearInterval(acceleratorInterval);
   }
 
-  var diff;
-
-  if (lastPos > -1) {
-    diff = (pos - lastPos) * -1;
-  } else {
-    diff = 0;
-  }
-
-  change += diff;
-
-  lastPos = pos;
-
-  var date = new Date();
-
-  realChange = (change / 6);
-
-  if (realChange > 0) {
-    realChange = Math.floor(realChange);
-  } else {
-    realChange = Math.ceil(realChange);
-  }
-  applyChange(date, realChange);
-
-  out.innerHTML = formatDate(date);
-};
-
-function formatDate(date) {
-  return paddy(date.getDate(), 2)
-    + "/"
-    + paddy((date.getMonth() + 1), 2)
-    + "/"
-    + date.getFullYear();
-}
-function paddy(subject, length, padChar) {
-  var pad_char = typeof padChar !== 'undefined' ? padChar : '0';
-  var pad = new Array(1 + length).join(pad_char);
-  return (pad + subject).slice(-pad.length);
-}
-
-var enableScroll = function (e) {
-  out.innerHTML = formatDate(applyChange(new Date(), realChange));
-
-  lastPos = e.screenY;
-
-  document.addEventListener('mousemove', moveListener);
-  document.addEventListener('touchmove', moveListener);
-};
-
-var disableScroll = function () {
-  document.removeEventListener('mousemove', moveListener);
-  document.removeEventListener('touchmove', moveListener);
-};
-
+})();
