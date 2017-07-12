8b239e2601d8967a2b911a076b54802f887f3f05
Julius Šakalys <julius.sakalys@gmail.com>
2017-07-12 18:45:37 +0300

    Initial

---
+++ range.js
@@ -0,0 +1,11 @@
+/**
+ * Created by tinklainis on 17.7.12.
+ */
+
+var picker = $('.sakalys-date-range');
+
+var from = picker.find('.skl-from'),
+    fromHandle = from.find('.handle');
+
+var to = picker.find('.skl-to'),
+    toHandle = to.find('.handle');
