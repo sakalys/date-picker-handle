efb090b5359f99074d65aa8614fce3bf2597078e
Julius Šakalys <julius.sakalys@gmail.com>
2017-07-31 22:19:07 +0300

    latest

--- dist/slider.js
+++ dist/slider.js
@@ -70,104 +70,170 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-var helpers_1 = __webpack_require__(1);
-var picker = document.querySelector('.sakalys-date-range');
-var from = picker.querySelector('.skl-from'), fromHandle = from.querySelector('.handle'), to = picker.querySelector('.skl-to'), toHandle = to.querySelector('.handle');
-var out = document.querySelector('.testFrom');
-var daysFromToday = 0, moveTotal = 0, stackedMove = 0, acceleratorInterval = null, currentPosition;
-function setupHandleElement(el) {
-    function stop(e) {
-        e.preventDefault();
+const helpers_1 = __webpack_require__(1);
+class ValueSlider {
+    constructor(el) {
+        this.el = el;
+        this.divider = 8;
+        this.date = new Date;
+        this.initialType = el.type;
+        this.value = el.value;
+        this._init();
     }
-    el.addEventListener('dragstart', stop);
-    el.addEventListener('touchstart', stop);
-}
-setupHandleElement(fromHandle);
-setupHandleElement(toHandle);
-function pressedCb(e) {
-    enableScroll(e);
-}
-function unpressedCb() {
-    disableScroll();
-}
-fromHandle.addEventListener('mousedown', pressedCb);
-fromHandle.addEventListener('touchstart', pressedCb);
-function moveListener(e) {
-    currentPosition = e.screenY;
-    moveTotal += stackedMove;
-    // stackedMove = 0;
-    daysFromToday = moveTotal / 30;
-    if (daysFromToday > 0) {
-        daysFromToday = Math.floor(daysFromToday);
+    _init() {
+        this.el.type = 'hidden';
+        this.wrapper = this._createWrapper(this.el);
+        this.handles = this._createHandles();
+        this.wrapper.appendChild(this.handles.year);
+        this.wrapper.appendChild(document.createTextNode('-'));
+        this.wrapper.appendChild(this.handles.month);
+        this.wrapper.appendChild(document.createTextNode('-'));
+        this.wrapper.appendChild(this.handles.day);
+        this.el.parentElement.insertBefore(this.wrapper, this.el);
+        this._updateText();
     }
-    else {
-        daysFromToday = Math.ceil(daysFromToday);
+    _createWrapper(el) {
+        const container = document.createElement('div');
+        el.className.split(' ').forEach(className => container.classList.add(className));
+        return container;
     }
-    out.innerHTML = formatDate(helpers_1.prepareDate(daysFromToday));
-}
-function formatDate(date) {
-    return paddy(String(date.getDate()), 2)
-        + "/"
-        + paddy(String(date.getMonth() + 1), 2)
-        + "/"
-        + date.getFullYear();
-}
-function paddy(subject, length, padChar) {
-    if (padChar === void 0) { padChar = '0'; }
-    var pad = new Array(1 + length).join(padChar);
-    return (pad + subject).slice(-pad.length);
-}
-function enableScroll(e) {
-    document.addEventListener('mouseup', unpressedCb);
-    document.addEventListener('touchend', unpressedCb);
-    document.addEventListener('touchcancel', unpressedCb);
-    out.innerHTML = formatDate(helpers_1.prepareDate(daysFromToday));
-    currentPosition = e.screenY;
-    document.addEventListener('mousemove', moveListener);
-    document.addEventListener('touchmove', moveListener);
-    startAccelerator();
-}
-function disableScroll() {
-    document.removeEventListener('mousemove', moveListener);
-    document.removeEventListener('touchmove', moveListener);
-    stopAccelerator();
-}
-function startAccelerator() {
-    function getCurrentPosition() {
-        return currentPosition;
+    _setupHandle(handle) {
+        this._on(handle, 'mousedown', this._getSliderCb(handle).bind(this));
     }
-    var samplesPerSecond = 30;
-    var previousPos = getCurrentPosition();
-    var mediumVel = 1;
-    acceleratorInterval = setInterval(function () {
-        var velocity = 1;
-        var thisPos = getCurrentPosition();
-        var diff = previousPos - thisPos;
-        var absDiff = Math.abs(diff);
-        if (absDiff < 8) {
-            velocity = 1;
+    static moveCb(e) {
+        ValueSlider.mousePosition = e.screenY;
+    }
+    _on(el, events, cb) {
+        events.split(' ').forEach((eventName) => {
+            el.addEventListener(eventName, cb);
+        });
+    }
+    _off(el, events, cb) {
+        events.split(' ').forEach((eventName) => {
+            el.removeEventListener(eventName, cb);
+        });
+    }
+    _getSliderCb(handle) {
+        return (e) => {
+            e.preventDefault();
+            let moveListener = ValueSlider.moveCb.bind(this);
+            const _stopSlider = () => {
+                this._off(document, 'mousemove', moveListener);
+                this._off(document, 'mouseup', boundStopListener);
+                this.stopClock();
+            };
+            const boundStopListener = _stopSlider.bind(this);
+            this._on(document, 'mousemove', moveListener);
+            this._on(document, 'mouseup', boundStopListener);
+            ValueSlider.mousePosition = e.screenY;
+            this.startClock(handle);
+        };
+    }
+    _throttleUpdate(cb) {
+        cb.apply(this);
+    }
+    static getCurrentPosition() {
+        return ValueSlider.mousePosition;
+    }
+    startClock(handle) {
+        this.tickData = {
+            mediumVel: 1,
+            samplesPerSecond: 30,
+            previous: {
+                velocity: 0,
+                position: ValueSlider.getCurrentPosition()
+            },
+            handle: handle
+        };
+        this.acceleratorInterval = setInterval(this._tick.bind(this), 1000 / this.tickData.samplesPerSecond);
+    }
+    _tick() {
+        const currentPosition = ValueSlider.getCurrentPosition();
+        const diff = this.tickData.previous.position - currentPosition;
+        const absDiff = Math.abs(diff);
+        let direction = 0;
+        if (diff > 0) {
+            direction = 1;
         }
-        else if (absDiff < 22) {
-            velocity = 10;
+        else if (diff < 0) {
+            direction = -1;
         }
-        else if (absDiff < 50) {
-            velocity = 300;
+        let velocity = (absDiff == 0 ? 0 : 1) * direction;
+        let newMedium = this._calculateNewMediumVelocity(velocity);
+        this.tickData.mediumVel = Math.floor(newMedium * 100) / 100;
+        if (Math.abs(this.tickData.mediumVel) < 1) {
+            this.tickData.mediumVel = 0;
         }
-        else if (absDiff < 90) {
-            velocity = 1000;
-        }
-        var newMedium = (3 * mediumVel + velocity) / 4;
-        mediumVel = Math.floor(newMedium * 100) / 100;
-        previousPos = thisPos;
-        stackedMove = mediumVel - 1;
-    }, 1000 / samplesPerSecond);
-}
-function stopAccelerator() {
-    if (!acceleratorInterval) {
-        return;
+        this.tickData.previous.position = currentPosition;
+        // handle.pixelsMoved = handle.pixelsMoved + diff;
+        this._throttleUpdate(() => {
+            // this.tickData.handle.updated(Math.floor(handle.pixelsMoved / this.divider));
+            this._updateText();
+        });
+        this.tickData.previous.velocity = this.tickData.mediumVel;
     }
-    clearInterval(acceleratorInterval);
+    _calculateNewMediumVelocity(currentVelocity) {
+        return this.tickData.previous.velocity == 0 ? Math.floor(currentVelocity) : ((this.tickData.mediumVel + currentVelocity * 2) / 3);
+    }
+    stopClock() {
+        if (!this.acceleratorInterval) {
+            return;
+        }
+        clearInterval(this.acceleratorInterval);
+    }
+    _createHandles() {
+        let handles = {};
+        ['day', 'month', 'year'].forEach((handleName) => {
+            const element = document.createElement('a');
+            element.classList.add(handleName);
+            Object.assign(element.style, {
+                cursor: 'ns-resize',
+            });
+            handles[handleName] = element;
+        });
+        let year = handles.year;
+        let month = handles.month;
+        let day = handles.day;
+        this._setupHandle(year);
+        this._setupHandle(month);
+        this._setupHandle(day);
+        let [yearText, monthText, dayText] = this.value.split('-');
+        this.date.setFullYear(parseInt(yearText));
+        this.date.setMonth(parseInt(monthText) - 1);
+        this.date.setDate(parseInt(dayText));
+        let val;
+        // YEAR
+        val = parseInt(yearText);
+        year.pixelsMoved = Math.floor(val * this.divider);
+        year.updated = (newVal) => {
+            this.date.setFullYear(newVal);
+        };
+        // MONTH
+        val = parseInt(monthText);
+        month.pixelsMoved = Math.floor(val * this.divider);
+        month.updated = (newVal) => {
+            if (newVal < 1) {
+                month.compensate = -1;
+            }
+            else if (newVal > 12) {
+                month.compensate = 1;
+            }
+            this.date.setMonth(newVal - 1);
+        };
+        val = parseInt(dayText);
+        day.pixelsMoved = Math.floor(val * this.divider);
+        day.updated = (newVal) => {
+            this.date.setDate(newVal);
+        };
+        return handles;
+    }
+    _updateText() {
+        this.handles.year.innerText = helpers_1.paddy(String(this.date.getFullYear()), 4);
+        this.handles.month.innerText = helpers_1.paddy(String(this.date.getMonth() + 1), 2);
+        this.handles.day.innerText = helpers_1.paddy(String(this.date.getDate()), 2);
+    }
 }
+new ValueSlider(document.querySelector('.from'));
 
 
 /***/ }),
@@ -178,11 +244,24 @@ function stopAccelerator() {
 
 Object.defineProperty(exports, "__esModule", { value: true });
 function prepareDate(dayDiff) {
-    var date = new Date();
+    const date = new Date();
     date.setTime(date.getTime() + (dayDiff * 1000 * 60 * 60 * 24));
     return date;
 }
 exports.prepareDate = prepareDate;
+function paddy(subject, length, padChar = '0') {
+    const pad = new Array(1 + length).join(padChar);
+    return (pad + subject).slice(-pad.length);
+}
+exports.paddy = paddy;
+function formatDate(date) {
+    return paddy(String(date.getDate()), 2)
+        + "/"
+        + paddy(String(date.getMonth() + 1), 2)
+        + "/"
+        + date.getFullYear();
+}
+exports.formatDate = formatDate;
 
 
 /***/ })
